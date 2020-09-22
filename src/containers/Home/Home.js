810d587be29a80777e403b37a361271e90288e94
Joshua Corpin <jkcorpin@gmail.com>
2020-09-22 14:30:36 +0800

    Refactor

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,22 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import LanguageIcon from "@material-ui/icons/Language";
-import RoomIcon from "@material-ui/icons/Room";
-import AccessTimeIcon from "@material-ui/icons/AccessTime";
-import CallIcon from "@material-ui/icons/Call";
 import Spinner from "../../components/Spinner/Spinner";
+import CountryCard from "../../components/CountryCard/CountryCard";
 
 const useStyles = makeStyles({
   countryCode: {
@@ -26,21 +16,6 @@ const useStyles = makeStyles({
   },
   containerMargin: {
     marginTop: "20px",
-  },
-  cardMedia: {
-    height: "250px",
-  },
-
-  icons: {
-    color: "#F75B00",
-  },
-  icons__text: {
-    paddingLeft: "5px",
-  },
-  alignIcons: {
-    display: "flex",
-    alignItems: "center",
-    marginTop: "8px",
   },
 });
 
@@ -88,84 +63,7 @@ function Home() {
   }, []);
 
   const countryCard = filterDisplay.map((country) => {
-    return (
-      <Grid item sm={12} md={4} key={country.alpha3Code}>
-        <Link to={`${country.name.split(" ").join("")}`}>
-          <Card>
-            <CardActionArea>
-              <CardMedia
-                image={country.flag}
-                title="country flag"
-                className={classes.cardMedia}
-              />
-              <CardContent>
-                <Grid container spacing={2}>
-                  <Grid item xs={6}>
-                    <Typography
-                      className={classes.alignIcons}
-                      variant="subtitle1"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      <LanguageIcon className={classes.icons} />
-                      <span className={classes.icons__text}>
-                        {`${country.name} (${country.alpha2Code})`}
-                      </span>
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography
-                      className={classes.alignIcons}
-                      variant="subtitle1"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      <RoomIcon className={classes.icons} />
-                      <span className={classes.icons__text}>
-                        {country.region === "Americas"
-                          ? "America"
-                          : country.region}
-                      </span>
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography
-                      className={classes.alignIcons}
-                      variant="subtitle1"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      <AccessTimeIcon className={classes.icons} />
-                      <span className={classes.icons__text}>
-                        {country.timezones[0]}
-                      </span>
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography
-                      className={classes.alignIcons}
-                      variant="subtitle1"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      <CallIcon className={classes.icons} />
-                      <span
-                        className={classes.icons__text}
-                      >{`+ ${country.callingCodes[0]}`}</span>
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </CardActionArea>
-            <CardActions style={{ display: "flex", justifyContent: "center" }}>
-              <Button fullWidth size="small" className={classes.icons}>
-                View More
-              </Button>
-            </CardActions>
-          </Card>
-        </Link>
-      </Grid>
-    );
+    return <CountryCard country={country} />;
   });
 
   let content = countryCard;
@@ -183,7 +81,6 @@ function Home() {
         fullWidth
         style={{ marginBottom: "20px" }}
         onChange={inputChangeHandler}
-        // value={input}
       />
       <Grid container spacing={3}>
         {content}
