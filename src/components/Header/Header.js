727385288e6a9b16bb0b712e1704f001a56627ba
Joshua Corpin <jkcorpin@gmail.com>
2020-09-22 14:39:32 +0800

    Added Link to Header Text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
+import { Container } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { Container } from "@material-ui/core";
 
 const useStyles = makeStyles({
   headerBGColor: {
     backgroundColor: "#F57E21",
+  },
+  headerTextColor: {
+    color: "#fff",
   },
 });
 
@@ -18,9 +22,11 @@ function Header() {
     <AppBar position="static" className={classes.headerBGColor}>
       <Container>
         <Toolbar variant="regular">
-          <Typography variant="h5" color="inherit">
-            CountryCodes
-          </Typography>
+          <Link to="/">
+            <Typography variant="h4" className={classes.headerTextColor}>
+              CountryCodes
+            </Typography>
+          </Link>
         </Toolbar>
       </Container>
     </AppBar>
